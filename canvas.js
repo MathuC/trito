3e40211ae4b469bed74a38fdb34e2ec69fe83b82
Mathusan Chandramohan <mathu99430@hotmail.com>
2020-07-17 20:02:31 -0400

    Started writing functions in canvas.js

--- canvas.js
+++ canvas.js
@@ -3,7 +3,16 @@ var c = canvas.getContext('2d');
 
 c.clearRect(0,0,400,600);
 
+
 c.fillRect(165,470,70,70);
 
 
+function bad() {
+	let type = Math.floor(Math.random() * 6);
+	alert(type);
+	if 
+}
+
+bad();
+	
 
