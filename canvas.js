7a5b7a43eec5e860eddc30c817de95165e62fb70
Mathusan Chandramohan <mathu99430@hotmail.com>
2020-07-17 22:25:49 -0400

    made the three squares for the game in the canvas

--- canvas.js
+++ canvas.js
@@ -2,18 +2,31 @@ var canvas = document.querySelector('canvas');
 var c = canvas.getContext('2d');
 
 c.clearRect(0,0,400,600);
-c.fillRect(165,470,70,70);
+
+c.fillRect(65,400,70,70);
+c.fillRect(165,400,70,70);
+c.fillRect(265,400,70,70);
+
 
 //key that player presses
-let key="None"; //keyt that player pressed
+let key="None"; 
 
-//which key the player presses
-document.addEventListener("keydown", function(event) {
+//game score
+let score=0;
+
+//which key the player presses. Everytime a key is pressed, the "key" var is updated with that value e.g: KeyS
+document.addEventListener("keydown", direction);
+function direction(event){
 	key = event.code;
 	alert(key);
-});
+}
 
 //draw the player
+function player(){
+	if (key == "KeyS") {
+	}
+	
+}
 
 //the function that defines the opponent
 function bad() {
