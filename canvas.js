2fb71ff2750aed6c5d5d731dccfe19ffd61064b3
Mathusan Chandramohan <mathu99430@hotmail.com>
2020-07-18 19:39:20 -0400

    Added the logic of the game. Game stops when the game is over. The animation stops.

--- canvas.js
+++ canvas.js
@@ -1,12 +1,9 @@
 var canvas = document.getElementById("trito");
 var c = canvas.getContext('2d');
 c.lineWidth=4;
-
-//key that player presses
-let key="KeyS"; 
-
+ 
 //game score
-let score=1000000;
+let score=0;
 
 //When score is a hundred multiple the score is displayed in big accompanied with this image
 var banner = new Image();
@@ -32,33 +29,33 @@ function space(event) {
 function drawPlayer(){
 	c.fillStyle="white"; // Without this line, the stuff can be seen through the player which is not very appealing to the eyes
 	if (key == "KeyA") {
-		c.fillRect(65,500,70,70);
+		//c.fillRect(65,500,70,70);
 		c.strokeRect(65,500,70,70);
 	} else if (key == "KeyS") {
-		c.fillRect(165,500,70,70);
+		//c.fillRect(165,500,70,70);
 		c.strokeRect(165,500,70,70);
 	} else if (key == "KeyD") {
-		c.fillRect(265,500,70,70);
+		//c.fillRect(265,500,70,70);
 		c.strokeRect(265,500,70,70);
 	} else if (key == "KeyJ") {
-		c.fillRect(83,500,35,70);
-		c.fillRect(183,500,35,70);
+		//c.fillRect(83,500,35,70);
+		//c.fillRect(183,500,35,70);
 		c.strokeRect(83,500,35,70);
 		c.strokeRect(183,500,35,70);
 	} else if (key == "KeyL") {
-		c.fillRect(183,500,35,70);
-		c.fillRect(283,500,35,70);
+		//c.fillRect(183,500,35,70);
+		//c.fillRect(283,500,35,70);
 		c.strokeRect(183,500,35,70);
 		c.strokeRect(283,500,35,70);
 	} else if (key == "KeyK") {
-		c.fillRect(83,500,35,70);
-		c.fillRect(283,500,35,70);
+		//c.fillRect(83,500,35,70);
+		//c.fillRect(283,500,35,70);
 		c.strokeRect(83,500,35,70);
 		c.strokeRect(283,500,35,70);
 	} else if (key == "Space") { //70/3 is ~23, not 18 but I did this so that the 1/3 square is visually different from the 1/2 square so that players see it easily
-		c.fillRect(91,500,18,70);
-		c.fillRect(191,500,18,70);
-		c.fillRect(291,500,18,70);
+		//c.fillRect(91,500,18,70);
+		//c.fillRect(191,500,18,70);
+		//c.fillRect(291,500,18,70);
 		c.strokeRect(91,500,18,70);
 		c.strokeRect(191,500,18,70);
 		c.strokeRect(291,500,18,70);
@@ -115,15 +112,15 @@ function drawScore(){
 	c.fillText(score,400,30);
 }
 
-//the function that defines the filled rectangles (obstacles)
+//the function that defines the type of the filled rectangles (wave obstacles)
 function rand() {
-	if (score>=0 && score<=10 ){
+	if (score>=0 && score<=5 ){
 		return Math.floor(Math.random() * 3); //only first
-	} else if (score>10 && score<=20) {
+	} else if (score>5 && score<=20) {
 		return (Math.floor(Math.random() * 3)+3);
 	} else if (score>20 && score<40) {
 		return Math.floor(Math.random() * 6);
-	} else if (score==40) { //
+	} else if (score==40) { //player needs to be introduced to this new wave at least once when the speed is still pretty low
 		return 6;
 	} else if (score>40){
 		return Math.floor(Math.random() * 7);
@@ -147,7 +144,7 @@ function rand2() {
 
 function drawHundred(y) { //when you reach mutliples of 100, you get a little break and your score is shown in the game in BIG instead of a wave
 	c.fillStyle="black";
-	c.font = "100px Lucida Console";
+	c.font = "80px Lucida Console";
 	c.textAlign = "center";
 	const temp= score+1-(score+1)%100; //So the score doesn't change in the middle of the screen
 	c.drawImage(banner,10,y-120,380,170);
@@ -164,6 +161,7 @@ let dy=15; //speed of the waves
 let ddy=8; //after a score of 50, the speed will increase, so this var is the acceleration of the waves till they reach a maximum speed
 let sep=325; //separation between the two waves goes from 400 to 350
 let type=1; //first wave is the easiest one (Answer: KeyS)
+let type2; //since there will be two waves on the same screen type and type2 are distinct types
 let count=true;//booleans for counting score
 let hundred=false;//boolean for the function drawHundred(y) to be called
 
@@ -189,11 +187,11 @@ function draw(){
 	}
 	if (y1>=sep && y2>y1) {
 		y2=-72;
-		type2= rand2();
+		type2= rand();
 	} else if (y1>=sep && filled2 == false) {
 		y2=-72;
 		filled2=true;
-		type2= rand2();
+		type2= rand();
 	} 
 	if (y2>=sep && y1>y2 && score%100!=99) {
 		y1=-72;
@@ -209,15 +207,29 @@ function draw(){
 		score = score+1;
 		count= true;
 	}
-	if(score>40 && dy<40 && score<1000){ //speeding up the waves after the player reach a score of 50, till the speed of the waves dy reaches a maximum
+	//speed starts to go up from score=40 to around score=800. 
+	if(score>40 && score<=1000){ //speeding up the waves after the player reach a score of 50, till the speed of the waves dy reaches a maximum
 		//dy=15+(score-40-(score-40)%ddy)/ddy; //mod so that the increase in speed happens incrementally
 		dy=15+Math.floor(0.8*(Math.sqrt(score-40)));
-	} else if (score>=1000){ //debugging purposes
+	} else if (score>1000){ //debugging purposes, helpful when trying out different start scores, especially when they are really big
 		dy=39;
 	}
 	drawPlayer(); //draws player on top of everyting else
-	console.log(dy); //debug
+	console.log("Speed:"+dy); //debugging
+
+	//GameOver
+	if (y1>430 && y1<570){
+		if ((type==0 && key!="KeyA") || (type==1 && key!="KeyS") || (type==2 && key!="KeyD") || (type==3 && key!="KeyJ") || (type==4 && key!="KeyL") || (type==5 && key!="KeyK") || (type==6 && key!="Space")) {
+			clearInterval(trito); //stops animation
+		}
+	}
+	if (y2>430 && y2<570) {
+		if ((type2==0 && key!="KeyA") || (type2==1 && key!="KeyS") || (type2==2 && key!="KeyD") || (type2==3 && key!="KeyJ") || (type2==4 && key!="KeyL") || (type2==5 && key!="KeyK") || (type2==6 && key!="Space")) {
+			clearInterval(trito); //stops animation
+		}
+	}
 }
 
 //game has an image drawn every 100 milliseconds
-let game = setInterval(draw,10);
+let key="KeyS";
+let trito = setInterval(draw,10);
