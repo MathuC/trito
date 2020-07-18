00eeec55848d13dada5ddd620ea3e5da07ac4dc1
Mathusan Chandramohan <mathu99430@hotmail.com>
2020-07-17 23:18:30 -0400

    added tenmplates to plan the game graphics

--- canvas.js
+++ canvas.js
@@ -1,12 +1,29 @@
 var canvas = document.querySelector('canvas');
 var c = canvas.getContext('2d');
+c.lineWidth=3;
 
 c.clearRect(0,0,400,600);
 
-c.fillRect(65,400,70,70);
-c.fillRect(165,400,70,70);
-c.fillRect(265,400,70,70);
+c.fillRect(91,500,18,70);
+c.fillRect(191,500,18,70);
+c.fillRect(291,500,18,70);
 
+
+c.strokeRect(83,500,35,70);
+c.strokeRect(183,500,35,70);
+c.strokeRect(283,500,35,70);
+
+
+c.strokeRect(65,500,70,70);
+c.strokeRect(165,500,70,70);
+c.strokeRect(265,500,70,70);
+//template trials 
+/*
+c.font = "15px Lucida Console";
+c.textAlign = "right";
+c.fillText("Score",400,15);
+c.fillText(33,400,30);
+*/
 
 //key that player presses
 let key="None"; 
@@ -17,15 +34,31 @@ let score=0;
 //which key the player presses. Everytime a key is pressed, the "key" var is updated with that value e.g: KeyS
 document.addEventListener("keydown", direction);
 function direction(event){
-	key = event.code;
-	alert(key);
+	if (event.code== "KeyA" || event.code== "KeyS" || event.code== "KeyD" || event.code== "KeyQ" || event.code== "KeyW" || event.code== "KeyE"){
+		key = event.code;
+		alert(key); //debugging
+	}
 }
 
 //draw the player
-function player(){
-	if (key == "KeyS") {
+function drawPlayer(){
+	if (key == "KeyA") {
+		c.fillRect(65,500,70,70);
+	} else if (key == "KeyS") {
+		c.fillRect(165,500,70,70);
+	} else if (key == "KeyD") {
+		c.fillRect(265,500,70,70);
+	} else if (key == "KeyQ") {
+		c.fillRect(83,500,36,70);
+		c.fillRect(183,500,36,70);
+	} else if (key == "KeyE") {
+		c.fillRect(183,500,36,70);
+		c.fillRect(283,500,36,70);
+	} else if (key == "KeyW") {
+		c.fillRect(91,500,18,70);
+		c.fillRect(191,500,18,70);
+		c.fillRect(291,500,18,70);
 	}
-	
 }
 
 //the function that defines the opponent
