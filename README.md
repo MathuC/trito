a9d8c5e6e570ab8104b38872ad187df9d15fd366
Mathusan Chandramohan <mathu99430@hotmail.com>
2020-07-22 06:36:24 -0400

    I completely changed the controls so I also changed the instructions and the instructions appear on the canvas now instead of below the game. The controlls have only 4 keys now WASD like normal computer games.

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 #Trito
 
 Trito is a simple javascript canvas game to play on computers. I thought of the idea and design for this simple game for starting to learn Javascript and I made it exculsively using documentation online (mostly pdf's).
-banner.png, corona.wav, gameOver.wav and point.wav were entirely made by me.
+banner.png, instructions.png corona.wav, gameOver.wav and point.wav were entirely made by me.
 
 Updates:
 1. I started coding this project on the 16th of July 2020.I need to finish this on the 19th of July 2020 at 11:59PM (bet).
 
 2. I finished the game on the 18th at night but want to add more features, like the leaderboard, so I will still be working on this after the 19th (won the bet).
+
+3.Completely changed the controls after a friend who tested my game told me about keyup event in JS, which made me go back to my original plan of using WASD as controls instead of 7 keys (A,S,D,J,K,L,Space) (since I first erroneously thought there was only a keydown event and that there was no way of knowing if a key was "being pressed")
--- canvas.js
+++ canvas.js
@@ -1,9 +1,13 @@
 var canvas = document.getElementById("trito");
+
 var c = canvas.getContext('2d');
+
 
 //When score is a fifty multiple the score is displayed in big accompanied with this image
 var banner = new Image();
 banner.src = "img/banner.png";
+var instructions = new Image();
+
 
 //load audio fileSize
 let hit = new Audio();
@@ -18,7 +22,12 @@ point.src= "audio/point.wav";
 let clap= new Audio();
 clap.src="audio/clap.wav";
 
-
+let trito; //global variable that will store the interval (game loop)
+let player; //global variable which stores the position of the player 
+let a; //these are the keys and they are false since they are up.
+let d;
+let s;
+let w;
 let score=0;//game score
 let game= false;//if the game has started, need this since if not, everytime the trisected square command would be received, it would restart the whole game :/
 let y1 = -72; //position of the first wave of filled rectangle, there will be sometimes two waves of rectangle at the same time on the screen so two sets of variables are necessary for some
@@ -36,12 +45,43 @@ let fifty=false;//boolean for the function drawFfity(y) to be called
 let flashing= false; //if flashing text at the end is flashing
 let name; //name of the player
 
-let key; //global variable which stores the last key you pressed
-let trito; //global variable that will store the interval (game loop)
 
-startScreen(); //BEGINNING OF EVERYTHING, then addEventListener starts the whole game
+
+
+//BEGINNING OF EVERYTHING, then addEventListener starts the whole game
+instructions.onload= function(){ //if I don't do this and use function startscreen(); alone instead, then the canvas loads before the image (instructions.png) loads and the image is not here. Write "startScreen();" instead of this big block and see for yourself
+	startScreen();
+} 
+instructions.src = "img/instructions.png"; // It has to be in this order for the image to load and work: 1. var img = new Image() 2. img.onload = function(){ something you want to do} 3.img.src=img/img.png
+
+
+
+
+
+
+//The start screen
+function startScreen(){
+	c.fillStyle="black";
+	c.font = "bold 25px Lucida Console"; 
+	fontMac("bold 25px");
+	c.textAlign = "center";
+	c.fillText("Press  Space  to start",200,300);
+	c.font = "10px Lucida Console";
+	fontMac("bold 10px");
+	c.fillText("Read the instructions below if it's your first time.",200,330);
+	c.lineWidth=3;
+	c.strokeRect(118,280,120,26);
+	c.lineWidth=3;
+	c.drawImage(instructions,97,400,200,140); //smaller
+	//c.drawImage(instructions,75,400,250,180); //bigger 
+}
+
 
 function reset(){ //If the player wants to play the game again
+	a=false; 
+	d=false;
+	s=false;
+	w=false;
 	score=0;
 	y1 = -72; 
 	y2 = -72; 
@@ -60,17 +100,39 @@ function reset(){ //If the player wants to play the game again
 	}
 }
 
-//which key the player presses. Everytime a key is pressed, the "key" var is updated with that value e.g: KeyS
-document.addEventListener("keydown", direction);
-function direction(event){
-	if (event.code== "KeyA" || event.code== "KeyS" || event.code== "KeyD" || event.code== "KeyJ" || event.code== "KeyK" || event.code== "KeyL" || event.code=="Space"){
-		key = event.code;
-		if (event.code=="Space" && game==false) {
-			reset(); //If the player is playing multiple games
-			go.pause();
-			game = true;
-			gameStart(); 
-		}
+
+
+//When a key is pressed down
+document.addEventListener("keydown", down); //the event is sent (returned) to the function down
+function down(event){ //function down receives the event sent by addEventListener as an argument
+	if (event.code=="KeyA"){
+		a=true;
+	} else if (event.code=="KeyD") {
+		d=true;
+	} else if (event.code=="KeyS") {
+		s=true;
+	} else if (event.code=="KeyW") {
+		w=true;
+	}
+	if (event.code=="Space" && game==false) {
+		reset(); //If the player is playing multiple games
+		go.pause();
+		game = true;
+		gameStart(); 
+	}
+}
+
+//When a key is let go
+document.addEventListener("keyup", up);
+function up(event){
+	if (event.code=="KeyA"){
+		a=false;
+	} else if (event.code=="KeyD") {
+		d=false;
+	} else if (event.code=="KeyS") {
+		s=false;
+	} else if (event.code=="KeyW") {
+		w=false;
 	}
 }
 
@@ -89,57 +151,69 @@ function fontMac(size){
 	}
 }
 
-//The start screen
-function startScreen(){
-	c.fillStyle="black";
-	c.font = "bold 25px Lucida Console"; 
-	fontMac("bold 25px");
-	c.textAlign = "center";
-	c.fillText("Press  Space  to start",200,300);
-	c.font = "10px Lucida Console";
-	fontMac("bold 10px");
-	c.fillText("Read the instructions below if it's your first time.",200,330);
-	c.lineWidth=3;
-	c.strokeRect(118,280,120,26);
-}
 
 //This is the nearly the whole game and it has an image drawn every 10 milliseconds
 function gameStart(){
-	key="KeyS"; //player's square starts in the middle by default
+	a=false; 
+	d=false;
+	s=false;
+	w=false; //player's square starts in the middle by default
 	track.play()
 	track.loop=true;
 	trito = setInterval(draw,10); 
 }
 
+//I started this game with 7 controls ASD for the square position and JKL for the half-square position and space for the trisected squares
+//After a friend told me that keyup is a thing in JS, something I overlooked I decided to go back to my original plan when starting this game and to only use 4 controls A,S,D,W like the typical games which will make the learning curve more shallow
+function position(){
+	if (w==true){
+		player="3";
+	} else if (a==true && d==false && s==true && w==false){
+		player="l2";
+	} else if (a==false && d==true && s==true && w==false){
+		player="r2";
+	} else if (a==false && d==false && s==true && w==false){ 
+		player="m2";
+	} else if (a==true && d==false && s==false && w==false){
+		player="l";
+	} else if (a==false && d==true && s==false && w==false){
+		player="r";
+	} else if (a==false && d==false && s==false && w==false){
+		player="m";
+	} //if both a and d are pressed down at the same time, then the past position is kept
+}
+
+
+
 //draws the player
 function drawPlayer(){
 	c.lineWidth=4;
 	c.fillStyle="white"; // Without this line, the stuff can be seen through the player which is not very appealing to the eyes
-	if (key == "KeyA") {
+	if (player == "l") {
 		c.fillRect(65,500,70,70);
 		c.strokeRect(65,500,70,70);
-	} else if (key == "KeyS") {
+	} else if (player == "m") {
 		c.fillRect(165,500,70,70);
 		c.strokeRect(165,500,70,70);
-	} else if (key == "KeyD") {
+	} else if (player == "r") {
 		c.fillRect(265,500,70,70);
 		c.strokeRect(265,500,70,70);
-	} else if (key == "KeyJ") {
+	} else if (player == "l2") {
 		c.fillRect(83,500,35,70);
 		c.fillRect(183,500,35,70);
 		c.strokeRect(83,500,35,70);
 		c.strokeRect(183,500,35,70);
-	} else if (key == "KeyL") {
+	} else if (player == "r2") {
 		c.fillRect(183,500,35,70);
 		c.fillRect(283,500,35,70);
 		c.strokeRect(183,500,35,70);
 		c.strokeRect(283,500,35,70);
-	} else if (key == "KeyK") {
+	} else if (player == "m2") {
 		c.fillRect(83,500,35,70);
 		c.fillRect(283,500,35,70);
 		c.strokeRect(83,500,35,70);
 		c.strokeRect(283,500,35,70);
-	} else if (key == "Space") { //70/3 is ~23, not 18 but I did this so that the 1/3 square is visually different from the 1/2 square so that players see it easily
+	} else if (player == "3") { //70/3 is ~23, not 18 but I did this so that the 1/3 square is visually different from the 1/2 square so that players see it easily
 		c.fillRect(91,500,18,70);
 		c.fillRect(191,500,18,70);
 		c.fillRect(291,500,18,70);
@@ -277,17 +351,18 @@ function draw(){
 		dy=18+Math.floor(0.4*(Math.sqrt(score-30))); //using a squrt function made a lot of sense since you want the speed to increase 
 		//quickly at the beginning so it's not boring but you want it to stop increasing so quickly towards the end since the difficulty increase between 15 and 20 is a lot lower than the difficulty increase between 30 and 35
 	} 
+	position(); //according to what keys are down/up this says which position the player has to be and this current funtion draws the player accordingly to the position
 	drawPlayer(); //draws player on top of everyting else
 	console.log("Speed:"+dy); //debugging
 
 	//GameOver
 	if (y1>430 && y1<570){ 
-		if ((type==0 && key!="KeyA") || (type==1 && key!="KeyS") || (type==2 && key!="KeyD") || (type==3 && key!="KeyJ") || (type==4 && key!="KeyL") || (type==5 && key!="KeyK") || (type==6 && key!="Space")) {
+		if ((type==0 && player!="l") || (type==1 && player!="m") || (type==2 && player!="r") || (type==3 && player!="l2") || (type==4 && player!="r2") || (type==5 && player!="m2") || (type==6 && player!="3")) {
 			gameOver(); //stops animation
 		}
 	}
 	if (y2>432 && y2<570) {
-		if ((type2==0 && key!="KeyA") || (type2==1 && key!="KeyS") || (type2==2 && key!="KeyD") || (type2==3 && key!="KeyJ") || (type2==4 && key!="KeyL") || (type2==5 && key!="KeyK") || (type2==6 && key!="Space")) {
+		if ((type2==0 && player!="l") || (type2==1 && player!="m") || (type2==2 && player!="r") || (type2==3 && player!="l2") || (type2==4 && player!="r2") || (type2==5 && player!="m2") || (type2==6 && player!="3")) {
 			gameOver(); //stops animation
 		}
 	}
@@ -315,6 +390,9 @@ function gameOver(){
 	setTimeout(function(){
 		if(name == null){
 			name = prompt("Enter your name/alias for the leaderboard (one word)");
+			if (name==""){
+				name=null;
+			}
 		}
 		highScore();}
 		,1800); 
@@ -381,6 +459,11 @@ Big error3:
 I did not make a boolean "game" which checks if game has started (spacebar has been pushed or not) or not to eliminate the 
 possibility of one key to be used for two things (trisected square & gamestart) in the 
 eventlistener direction.
+
+Big error4: JS doesn't doesn't make an array of references when you define an array of variables it 
+makes an array of the values of those variables. So if you change something in a position of the array
+like array[0]=100, it won't change the variable that you assigned at position-0 to 100, it will just change
+the array
 
 */
 
